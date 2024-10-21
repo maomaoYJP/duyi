355db7f84a1c88911e4706cf8021bb5cdd456fb3
Maomao <Maomao@gdufe.com>
2024-10-22 00:10:05 +0800

    add(头像组件)

---
+++ my-site/src/components/Avatar.vue
@@ -0,0 +1,30 @@
+<template>
+  <img
+    class="img"
+    :src="url"
+    :style="{ width: size + 'px', height: size + 'px' }"
+  />
+</template>
+
+<script>
+export default {
+  props: {
+    url: {
+      type: String,
+      required: true,
+    },
+    size: {
+      type: String,
+      default: "50",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.img {
+  border-radius: 50%;
+  display: block;
+  object-fit: cover;
+}
+</style>
