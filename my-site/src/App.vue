8d448112244d3b9bf9898e9df96752ce37d091ea
Maomao <Maomao@gdufe.com>
2024-10-23 00:09:20 +0800

    add(图标组件)

--- my-site/src/App.vue
+++ my-site/src/App.vue
@@ -2,12 +2,20 @@
   <div>
     <h1>App组件</h1>
     <Avatar :url="'https://picsum.photos/200/300'" :size="200"></Avatar>
+    <Icon :type="'arrowDown'"></Icon>
   </div>
 </template>
 <script>
 import Avatar from "./components/Avatar.vue";
+import Icon from "./components/icon.vue";
 export default {
-  components: { Avatar },
+  components: { Avatar, Icon },
   name: "App",
 };
 </script>
+<style scoped>
+.iconfont {
+  color: red;
+  font-size: 30px;
+}
+</style>
---
+++ my-site/src/components/icon.vue
@@ -0,0 +1,28 @@
+<template>
+  <i class="iconfont" :class="iconType"></i>
+</template>
+
+<script>
+const iconMap = {
+  arrowUp: "icon-jiantou-copy",
+  arrowDown: "icon-jiantou",
+};
+
+export default {
+  props: {
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    iconType() {
+      return iconMap[this.type];
+    },
+  },
+};
+</script>
+
+<style scoped>
+@import "//at.alicdn.com/t/c/font_4720046_ehayhfeqmw9.css";
+</style>
